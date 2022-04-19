091083b30563990b9a68e31af657cb1b13d7607e
暮歌丶 <329167139@qq.com>
2022-04-19 15:16:11 +0800

    _

--- src/convert.rs
+++ src/convert.rs
@@ -1,5 +1,21 @@
 use std::mem;
 
+/// Caller makes sure the safety
+#[macro_export]
+macro_rules! static_refs {
+	($($id:ident = $ref:expr;)+) => {
+		$(let $id = unsafe{ static_ref($ref) };)+
+	}
+}
+
+/// Caller makes sure the safety
+#[macro_export]
+macro_rules! static_refs_mut {
+	($($id:ident = $ref:expr;)+) => {
+		$(let $id = unsafe{ static_ref_mut($ref) };)+
+	}
+}
+
 /// Safety: Caller holds
 pub unsafe fn static_ref<T>(r:&T) -> &'static T {
 	mem::transmute(r)
@@ -8,4 +24,17 @@ pub unsafe fn static_ref<T>(r:&T) -> &'static T {
 /// Safety: Caller holds
 pub unsafe fn static_ref_mut<T>(r:&mut T) -> &'static mut T {
 	mem::transmute(r)
+}
+
+#[test]
+fn test() {
+	let n = 3;
+	static_refs!{
+		a = &2;
+		b = &n;
+	};
+	fn need_static(_:&'static i32) {}
+
+	need_static(a);
+	need_static(b);
 }
