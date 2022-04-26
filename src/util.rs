607845e9f545b62403b20904c7e01188818c242e
暮歌丶 <329167139@qq.com>
2022-04-27 00:39:29 +0800

    _

--- src/util.rs
+++ src/util.rs
@@ -10,10 +10,10 @@ pub fn time_test<R>(f: impl FnOnce() -> R) -> (Duration, R) {
 pub mod rate {
     use core::fmt;
     use std::fmt::Display;
-    use std::ops::Div;
-    use std::ops::Mul;
-	
+
     pub struct Rate<T, T2>(T, T2, u8);
+	pub struct RemainRate<T, T2>(T, T2, u8);
+
 
     pub trait RateTrait<T> {
         fn rate(self, other: T) -> f64;
@@ -68,11 +68,41 @@ pub mod rate {
         }
     }
 
+	impl<T, T2> fmt::Debug for RemainRate<T, T2>
+    where
+        T: Copy + RateTrait<T2>,
+        T2: Copy + RateTrait<T>,
+    {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(
+                f,
+                "{:.width$?}%",
+                100.0 - self.0.rate(self.1),
+                width = self.2 as usize
+            )
+        }
+    }	
+
+	impl<T, T2> Display for RemainRate<T, T2>
+    where
+        T: Copy + RateTrait<T2>,
+        T2: Copy + RateTrait<T>,
+    {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "{:?}", self)
+        }
+    }
+
     #[test]
     fn test() {
         println!("{:?}", Rate(0.5_f32, 1.0_f32, 2));
         println!("{:?}", Rate(0.5_f32, 1.0_f64, 2));
         println!("{:?}", Rate(0.34232_f64, 1.0_f64, 44));
         println!("{:?}", Rate(0.34232_f64, 1.0_f32, 44));
+
+        println!("{:?}", RemainRate(0.5_f32, 1.0_f32, 2));
+        println!("{:?}", RemainRate(0.5_f32, 1.0_f64, 2));
+        println!("{:?}", RemainRate(0.34232_f64, 1.0_f64, 44));
+        println!("{:?}", RemainRate(0.34232_f64, 1.0_f32, 44));
     }
 }
