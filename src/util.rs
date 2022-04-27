bdb448aaf4c6fbd3dbed63bd518cc8a4de0fd7f6
暮歌丶 <329167139@qq.com>
2022-04-28 04:05:19 +0800

    _

--- src/util.rs
+++ src/util.rs
@@ -26,6 +26,21 @@ pub mod rate {
         fn rate(self, other: T) -> f64;
     }
 
+    pub trait IsZero {
+        fn is_zero(self) -> bool;
+    }
+
+    impl IsZero for f32 {
+        fn is_zero(self) -> bool {
+            self == 0.0
+        }
+    }
+    impl IsZero for f64 {
+        fn is_zero(self) -> bool {
+            self == 0.0
+        }
+    }
+
     impl RateTrait<f32> for f32 {
         fn rate(self, other: f32) -> f64 {
             (self as f64).rate(other as f64)
@@ -53,22 +68,26 @@ pub mod rate {
     impl<T, T2> fmt::Debug for Rate<T, T2>
     where
         T: Copy + RateTrait<T2>,
-        T2: Copy + RateTrait<T>,
+        T2: Copy + RateTrait<T> + IsZero,
     {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            write!(
-                f,
-                "{:.width$?}%",
-                self.0.rate(self.1),
-                width = self.2 as usize
-            )
+            if self.1.is_zero() {
+                write!(f, "N/A")
+            } else {
+                write!(
+                    f,
+                    "{:.width$?}%",
+                    self.0.rate(self.1),
+                    width = self.2 as usize
+                )
+            }
         }
     }
 
     impl<T, T2> Display for Rate<T, T2>
     where
         T: Copy + RateTrait<T2>,
-        T2: Copy + RateTrait<T>,
+        T2: Copy + RateTrait<T> + IsZero,
     {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
             write!(f, "{:?}", self)
@@ -78,22 +97,26 @@ pub mod rate {
     impl<T, T2> fmt::Debug for RemainRate<T, T2>
     where
         T: Copy + RateTrait<T2>,
-        T2: Copy + RateTrait<T>,
+        T2: Copy + RateTrait<T> + IsZero,
     {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            write!(
-                f,
-                "{:.width$?}%",
-                100.0 - self.0.rate(self.1),
-                width = self.2 as usize
-            )
+            if self.1.is_zero() {
+                write!(f, "N/A")
+            } else {
+                write!(
+                    f,
+                    "{:.width$?}%",
+                    100.0 - self.0.rate(self.1),
+                    width = self.2 as usize
+                )
+            }
         }
     }
 
     impl<T, T2> Display for RemainRate<T, T2>
     where
         T: Copy + RateTrait<T2>,
-        T2: Copy + RateTrait<T>,
+        T2: Copy + RateTrait<T> + IsZero,
     {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
             write!(f, "{:?}", self)
@@ -102,6 +125,7 @@ pub mod rate {
 
     #[test]
     fn test() {
+        println!("{:?}", Rate(0.5_f32, 0.0_f32, 2));
         println!("{:?}", Rate(0.5_f32, 1.0_f32, 2));
         println!("{:?}", Rate(0.5_f32, 1.0_f64, 2));
         println!("{:?}", Rate(0.34232_f64, 1.0_f64, 44));
